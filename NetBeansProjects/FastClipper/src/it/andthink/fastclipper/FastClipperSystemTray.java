e5330e5e4b6d99671fb606bfe28555ee532d478b
anthink <andrea.merli@gmail.com>
2012-06-21 11:02:28 +0200

    modifiche al fast clipper About e gestione vqr

--- NetBeansProjects/FastClipper/src/it/andthink/fastclipper/FastClipperSystemTray.java
+++ NetBeansProjects/FastClipper/src/it/andthink/fastclipper/FastClipperSystemTray.java
@@ -1,10 +1,8 @@
 package it.andthink.fastclipper;
 
+import it.andthink.utils.Open;
+import it.andthink.utils.Clipboard;
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -13,12 +11,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 /**
  * Classe per la gestione della SystemTray del server
@@ -31,8 +23,9 @@ public class FastClipperSystemTray {
 	private TrayIcon trayIcon;
 	private Image imgClip;
 	private PopupMenu popupMenu;
-	private Clipboard clipbordella;
 	private File FoundFile = null;
+	private Open OpenUtil = new Open();
+	private Clipboard clipboard = new Clipboard();
 
 	/**
 	 * Istanzia il programma nella systemTray
@@ -84,26 +77,36 @@ public class FastClipperSystemTray {
 	 */
 	private PopupMenu createPopupMenu() {
 		PopupMenu p = new PopupMenu();
+
 		MenuItem SearchAndOpen = new MenuItem("Search and open");
-
 		MenuItem about = new MenuItem("About");
-
 		MenuItem esci = new MenuItem("Quit");
 
-		//Azione per il click su Who
-
+		//Azione per il click su SearchAndOpen
 		SearchAndOpen.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				String text = getClipboard();
+				String text = clipboard.getClipboard();
 
-				File path = new File("C:/Sviluppo/PagoWEB/pago/");
+				File path = null;
+				boolean isVqr = false;
+				if (text.endsWith(".vqr")) {
+					path = new File("C:/Sviluppo/PagoWEB/pago/exe");
+					isVqr = true;
+				} else {
+					path = new File("C:/Sviluppo/PagoWEB/pago/");
+				}
+
 				searchAndOpen(path, text);
 
 				if (FoundFile != null) {
-					Edit(FoundFile);
+					if (isVqr) {
+						OpenUtil.launchVQInBrowser(FoundFile.getName());
+					} else {
+						OpenUtil.Edit(FoundFile);
+					}
 				}
 			}
 		});
@@ -115,28 +118,9 @@ public class FastClipperSystemTray {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JFrame frame = new JFrame("Per correre più veloci..."); 
-				frame.setLayout(null);
-				frame.setResizable(false);
-				frame.setIconImage(imgClip);
-				frame.setSize(613,433);
-				JTextArea clipboardContent= new JTextArea(getClipboard());
-				clipboardContent.setTabSize(1);
-				JScrollPane jsp = new JScrollPane(clipboardContent);
-				jsp.setBounds(3, 3, 600, 400);
-				frame.add(jsp);
-				
-				//Centro il frame nello schermo 
-				Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-				Dimension size = frame.getSize();
-				screenSize.height =	screenSize.height / 2;
-				screenSize.width = screenSize.width / 2;
-				size.height = size.height / 2;
-				size.width = size.width / 2;
-				int y = screenSize.height - size.height;
-				int x = screenSize.width - size.width;
-				frame.setLocation(x, y);
+				AboutFrame frame = new AboutFrame("Per correre più veloci...");
 
+				frame.getClipboardArea().setText(clipboard.getClipboard());
 				frame.setVisible(true);
 			}
 		});
@@ -155,41 +139,11 @@ public class FastClipperSystemTray {
 		//Aggiungo gli elemento al PopupMenu
 
 		p.add(SearchAndOpen);
-		p.addSeparator(); 
-		p.add(about); 
+		p.addSeparator();
+		p.add(about);
 		p.addSeparator();
 		p.add(esci);
 		return p;
-	}
-
-	private void Edit(File file) {
-		System.out.println(file.getAbsolutePath());
-		try {
-			if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-				String cmd = "rundll32 url.dll,FileProtocolHandler " + file.getCanonicalPath();
-
-				Runtime.getRuntime().exec(cmd);
-			} else {
-				Desktop.getDesktop().edit(file);
-			}
-		} catch (IOException ex) {
-			Logger.getLogger(FastClipperSystemTray.class.getName()).log(Level.SEVERE, null, ex);
-		}
-
-	}
-
-	private String getClipboard() {
-
-		Transferable content = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
-		String text = "";
-
-		try {
-			if (content != null && content.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-				text = (String) content.getTransferData(DataFlavor.stringFlavor);
-			}
-		} catch (UnsupportedFlavorException | IOException excep) {
-		}
-		return text;
 	}
 
 	/**
@@ -201,9 +155,8 @@ public class FastClipperSystemTray {
 		public void mouseClicked(MouseEvent e) {
 			if (e.getButton() == 1) {
 
-
 				//Pulsante sinistro del mouse crea file dai contenuti clipboard
-				String text = getClipboard();
+				String text = clipboard.getClipboard();
 
 				try {
 					// Create temp file.
@@ -215,7 +168,7 @@ public class FastClipperSystemTray {
 						out.write(text);
 					}
 					if (temp != null) {
-						Edit(temp);
+						OpenUtil.Edit(temp);
 					}
 
 				} catch (IOException exc) {
